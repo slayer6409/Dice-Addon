80e1d0cd41e1a868603d0a2d8c73b96802651a87
slayer6409 <andrews.webs6409@gmail.com>
2024-11-08 23:10:20 -0800

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -4,7 +4,7 @@ using DiceAddon.Effects;
 namespace DiceAddon
 {
     [BepInPlugin("Slayer6409.DiceAddon", "DiceAddon", "0.0.1")]
-    [BepInDependency("Theronguard.EmergencyDice")]
+    [BepInDependency("Theronguard.EmergencyDice")] // This is the original author, I just kept it since certain mods depended on it and it would break those if I changed it
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
